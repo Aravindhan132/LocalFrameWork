2aeb10cadfae72ed0e4ee6ac154dfb360c86fa85
“aravindhan.natarajan” <aravindhan.natarajan@zohocorp.com>
2019-05-23 22:03:20 +0530

    Initial Commit

---
+++ LocalFrameWork/LocalFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  LocalFrameWork.h
+//  LocalFrameWork
+//
+//  Created by aravind-pt2199 on 23/05/19.
+//  Copyright © 2019 aravind-pt2199. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalFrameWork.
+FOUNDATION_EXPORT double LocalFrameWorkVersionNumber;
+
+//! Project version string for LocalFrameWork.
+FOUNDATION_EXPORT const unsigned char LocalFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFrameWork/PublicHeader.h>
+
+
---
+++ LocalFrameWorkTests/LocalFrameWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  LocalFrameWorkTests.swift
+//  LocalFrameWorkTests
+//
+//  Created by aravind-pt2199 on 23/05/19.
+//  Copyright © 2019 aravind-pt2199. All rights reserved.
+//
+
+import XCTest
+@testable import LocalFrameWork
+
+class LocalFrameWorkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
